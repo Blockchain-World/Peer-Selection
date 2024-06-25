05e6477ed9b000593e75d9db71e8da2f4d0e0004
Blockchain <166235604+Blockchain-train@users.noreply.github.com>
2024-06-26 00:45:35 +0800

    Update Utility.sol

--- PeerSelection/Utility.sol
+++ PeerSelection/Utility.sol
@@ -3,33 +3,6 @@ pragma solidity ^0.5.10;
 import "./altbn128.sol";
 
 library UTI {
-    
-    struct ERK {
-        uint position; // the position in KT
-        bytes32 erk_hash; // the hash value of erk stored on-chain
-    }
-    
-    struct SubmittedERK {
-        uint position; // the position index in KT
-        uint C1_X;   // C1.X
-        uint C1_Y;   // C1.Y
-        uint C2_X;   // C2.X
-        uint C2_Y;   // C2.Y
-    }
-    
-    struct SubmittedRK {
-        uint position; // the position in KT
-        bytes32 value; // the submitted rk value
-    }
-    
-    struct VPKEProof {
-        uint position; // the position is the index in KT
-        uint A_X; // A.X on BN128Curve
-        uint A_Y; // A.Y on BN128Curve
-        uint B_X; // B.X on BN128Curve
-        uint B_Y; // B.Y on BN128Curve
-        uint Z;   // Z
-    }
 
     struct Deliverer {
         address payable deliverer_address;
